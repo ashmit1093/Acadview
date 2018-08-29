982c41893a264cb72f39df4c47947f429cfb2ac0
ashmit <gbakshi22@gmail.com>
2018-08-29 22:26:51 +0530

    Correction in assignment four

--- assignment-four/SecondLargest.java
+++ assignment-four/SecondLargest.java
@@ -9,8 +9,9 @@ class SecondLargest
 
 		Scanner s = new Scanner(System.in);
 		int a = s.nextInt();
+	
 		int[] array1 = new int[a];
-		
+
 		System.out.println("Enter elements in array");	
 
 		for(int i=0; i<a; i++)
@@ -18,21 +19,21 @@ class SecondLargest
 			array1[i] = s.nextInt();
 		}
 		
-		int largest = 0;
-		int secondLargest = 0;
+		Arrays.sort(array1);
+		
+		System.out.println("After sorting");	
 
-		for (int i = 0; i < array1.length; i++) {
- 
-			if (array1[i] > largest) {
-				secondLargest = largest;
-				largest = array1[i];
- 
-			} else if (array1[i] > secondLargest) {
-				secondLargest = array1[i];
- 
-			}
+		for(int i=0; i<a; i++)
+		{
+			System.out.print(array1[i]+" ");
 		}
+		System.out.println("\n");
 
-		System.out.println("Second largest numbers is " + secondLargest);
+    
+		System.out.println("Second largest element is : "+ array1[a-2]);
+		
+		s.close();
+   
+
 	}
 }
